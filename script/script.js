1b2352eb28ec8f55f49b58e35b04abf9a588dfc9
Hector Kurtyanek <hkurtyanek@adaptavist.com>
2022-08-03 08:14:56 +0100

    add navbar

--- script/script.js
+++ script/script.js
@@ -1,2 +1,13 @@
-const header = document.querySelector(".grid-one");
-console.log(header);
+// selectors / variables
+const menu = document.querySelector(".menu");
+// Hamburger menu functionality
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+// Mobile Menu
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
